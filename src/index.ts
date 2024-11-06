f596e5dda5052a19d6f5813323db588cdc6f0ade
Timi Tayo <timilehin.ty@gmail.com>
2024-11-06 18:45:11 +0100

    chore: Rebuild

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,12 @@ import { prisma } from "./utils";
 
 async function main() {
   try {
-    // if (isPodcastDbOldOrMissing()) {
-    //   await downloadAndExtractDatabase();
-    //   await cleanupDatabase();
-    // }
+    if (isPodcastDbOldOrMissing()) {
+      await downloadAndExtractDatabase();
+      await cleanupDatabase();
+    }
     startReEnricher();
-    // enrichAll();
+    enrichAll();
   } catch (e) {
     console.error(`Error starting up enricher: ${e}`);
   }
